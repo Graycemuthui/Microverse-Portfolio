e92a18e31a6af709855cde49810edad97ad681e7
Graycemuthui <graycemuthui20@gmail.com>
2022-05-10 12:00:47 +0300

    Fix linters

--- javascript/form.js
+++ javascript/form.js
@@ -1,26 +1,25 @@
-const navigationBar = document.querySelector(".navigation-bar");
-const listItems = document.querySelectorAll(".list-item");
-const hamburger = document.querySelector(".hamburger");
-const crossmark = document.querySelector(".fa-times");
-const bars = document.querySelector(".fa-bars");
+const navigationBar = document.querySelector('.navigation-bar');
+const listItems = document.querySelectorAll('.list-item');
+const hamburger = document.querySelector('.hamburger');
+const crossmark = document.querySelector('.fa-times');
+const bars = document.querySelector('.fa-bars');
 
 function toggleHamburger() {
-  if (navigationBar.classList.contains("show-menu")) {
-    navigationBar.classList.remove("show-menu");
-    crossmark.style.display = "none";
-    bars.style.display = "block";
+  if (navigationBar.classList.contains('show-menu')) {
+    navigationBar.classList.remove('show-menu');
+    crossmark.style.display = 'none';
+    bars.style.display = 'block';
   } else {
-    navigationBar.classList.add("show-menu");
-    crossmark.style.display = "block";
-    bars.style.display = "none";
+    navigationBar.classList.add('show-menu');
+    crossmark.style.display = 'block';
+    bars.style.display = 'none';
   }
 }
 
-hamburger.addEventListener("click", toggleHamburger);
+hamburger.addEventListener('click', toggleHamburger);
 
-listItems.forEach( 
-  function(listItem) { 
-    listItem.addEventListener("click", toggleHamburger);
-  }
-)
-
+listItems.forEach(
+  (listItem) => {
+    listItem.addEventListener('click', toggleHamburger);
+  },
+);

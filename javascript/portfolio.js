e6efbe4ac9aec62ed1dd668110a7cbe0f3420ac6
Graycemuthui <graycemuthui20@gmail.com>
2022-05-11 23:13:17 +0300

    Add iamges and portfolio.js file

---
+++ javascript/portfolio.js
@@ -0,0 +1,53 @@
+const portfolioCards = [
+{
+card : 1,
+title: 'Tonic',
+title1: 'Tonic',
+image: 'images/snapshot1.svg',
+image1: 'images/tonic.svg',
+description: ` A daily selection of privately personalized reads; no accounts or
+            sign-ups required.`,
+languages: ['html', 'css', 'javascript'],
+specifics: ['CANOPY', 'Back End Dev', '2015'],
+specifics1: ['CANOPY', 'Back End Dev', '2015'],
+},
+{
+card : 2,
+title: 'Multi-Post Stories',
+title1: 'Multi-Post Stories',
+image: 'images/availability.svg',
+image1: 'images/multidesktop.svg',
+description: `  Experimental content creation feature that allows users to add to an
+            existing story over the course of a day without spamming their
+            friends.`,
+languages: ['html', 'css', 'javascript'],
+specifics: ['CANOPY', 'Back End Dev', '2015'],
+specifics2: ['FACEBOOK', 'Full Stack Dev', '2015'],
+},
+{
+card : 3,
+title: 'Tonic',
+title1: 'Facebook 360',
+image: 'images/snapshot3.svg',
+image1: 'images/tonicdesktop.svg',
+description: ` Exploring the future of media in Facebook's first Virtual Reality
+            app; a place to discover and enjoy 360 photos and videos on Gear VR.`,
+languages: ['html', 'css', 'javascript'],
+specifics: ['CANOPY', 'Back End Dev', '2015'],
+specifics2: ['FACEBOOK', 'Full Stack Dev', '2015'],
+},
+{
+card : 4,
+title: 'Multi-Post Stories',
+title1: 'Uber Navigation',
+image: 'images/availability.svg',
+image1: 'images/tonic.svg',
+description: `   A smart assistant to make driving more safe, efficient, and fun by
+            unlocking your most expensive computer: your car.`,
+languages: ['html', 'css', 'javascript'],
+specifics: ['CANOPY', 'Back End Dev', '2015'],
+specifics1: ['UBER', 'Lead Developer', '2018'],
+},
+];
+
+

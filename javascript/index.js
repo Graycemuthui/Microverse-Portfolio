7afa4465c7aa03631ecff3ff5be4e8c7e3b1d7f7
Paul-js-hub <hesponpoisson@gmail.com>
2022-05-12 00:44:16 +0300

    Add dynamically objects to the main page

---
+++ javascript/index.js
@@ -0,0 +1,106 @@
+const navigationBar = document.querySelector('.navigation-bar');
+const listItems = document.querySelectorAll('.list-item');
+const hamburger = document.querySelector('.hamburger');
+const crossmark = document.querySelector('.fa-times');
+const bars = document.querySelector('.fa-bars');
+
+function toggleHamburger() {
+  if (navigationBar.classList.contains('show-menu')) {
+    navigationBar.classList.remove('show-menu');
+    crossmark.style.display = 'none';
+    bars.style.display = 'block';
+  } else {
+    navigationBar.classList.add('show-menu');
+    crossmark.style.display = 'block';
+    bars.style.display = 'none';
+  }
+}
+
+hamburger.addEventListener('click', toggleHamburger);
+
+listItems.forEach(
+  (listItem) => {
+    listItem.addEventListener('click', toggleHamburger);
+  },
+);
+
+const portfolioCards = [
+  {
+    title: 'Tonic',
+    title1: 'Tonic',
+    image: 'images/snapshot1.svg',
+    image1: 'images/tonic.svg',
+    description: "A daily selection of privately personalized reads; no accounts or sign-ups required.",
+    languages: ["html", "css", "JavaScript"]
+    },
+    {
+    title: 'Multi-Post Stories',
+    title1: 'Multi-Post Stories',
+    image: 'images/availability.svg',
+    image1: 'images/multidesktop.svg',
+    description: "Experimental content creation feature that allows users to add to anexisting story over the course of a day without spamming their friends.",
+    languages: ["html", "css", "JavaScript"]
+    },
+    {
+    title: 'Tonic',
+    title1: 'Facebook 360',
+    image: 'images/snapshot3.svg',
+    image1: 'images/tonicdesktop.svg',
+    description: "Exploring the future of media in Facebook's first Virtual Realityapp; a place to discover and enjoy 360 photos and videos on Gear VR.",
+    languages: ["html", "css", "JavaScript"]
+    },
+    {
+    title: 'Multi-Post Stories',
+    title1: 'Uber Navigation',
+    image: 'images/snapshot4.svg',
+    image1: 'images/availability.svg',
+    description: "A smart assistant to make driving more safe, efficient, and fun byunlocking your most expensive computer: your car.",
+    languages: ["html", "css", "JavaScript"]
+    },
+];
+
+let demo = document.getElementById("portfolio large-portfolio");
+demo.innerHTML = portfolioCards.reduce((output, portfolio) => {
+  console.log("PROJECT", portfolioCards)
+  return (
+    output +
+    `
+    <article class="card large-card">
+        <img
+          class="card-image"
+          src="${portfolio.image}"
+          alt="A guy with an article beside his picture"
+        />
+        <img
+          class="large-image"
+          src="${portfolio.image1}"
+          alt="Some diagrams and a menu"
+        />
+        <div class="card-body">
+          <h2 class="small-heading">${portfolio.title}</h2>
+          <h2 class="large-heading">${portfolio.title1}</h2>
+          <div class="card-specs">
+            <span class="text-normal">CANOPY</span>
+            <span class="large-text">CANOPY</span>
+            <img src="./images/Counter.svg" alt="" class="counter" />
+            <span class="text-light">Back End Dev</span>
+            <span class="large-text__light">Back End Dev</span>
+            <img src="./images/Counter.svg" alt="" class="counter" />
+            <span class="text-light">2015</span>
+            <span class="large-text__light">2015</span>
+          </div>
+          <p class="small-para">
+            ${portfolio.description}
+          </p>
+          <p class="large-para">
+            ${portfolio.description}
+          </p>
+          <ul class="categories">
+          ${portfolio.languages.map(x => `<li>${x}</li>`)}
+          </ul>
+          <button class="btn card-button">See Project</button>
+        </div>
+      </article>
+  `
+  );
+}, "");

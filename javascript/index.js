1882250f1080bccbfab076fb2de2fa3701e29207
Graycemuthui <graycemuthui20@gmail.com>
2022-05-13 09:54:57 +0300

    Adjust height of popup window

--- javascript/index.js
+++ javascript/index.js
@@ -101,17 +101,7 @@ demo.innerHTML = portfolioCards.reduce((output, portfolio) => (
   `
 ), '');
 
-const modalCards = [
-{
-    title: 'Tonic',
-    title1: 'Tonic',
-    image: 'images/snapshot1.svg',
-    image1: 'images/tonic.svg',
-    description:`Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent.`,
-    languages: ['html', 'css', 'JavaScript'],
-},
-]
-
+const images = document.getElementsByClassName('')
 const closeButton = document.querySelector('#modal-close');
 const btn = document.querySelectorAll('[data-open]');
 const overlay = document.querySelector('.popup-overlay');

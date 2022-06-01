a0266d06b9d45c85da8e0a78ef737f858391a9ec
Graycemuthui <graycemuthui20@gmail.com>
2022-06-01 09:19:20 +0300

    Start over

---
+++ webpack-demo.2/webpack.config.js
@@ -0,0 +1,15 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    filename: "main.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "Output Management",
+    }),
+  ],
+};
